a624259490f3563e23897098fef6963ec9c4cd4e
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-17 14:57:52 +0600

    not working note method

--- ajax-project/app.js
+++ ajax-project/app.js
@@ -1,29 +1,71 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
-let p = document.querySelector("#name");
-// fetch(URL)
-//     .then(response => response.json())
-//     .then(data => {
-//         data.forEach(user => {
-//            p.innerHTML =  `${p.innerHTML} <br> Name: ${user.name}`
-//         });
-//     })
-//     .catch(err => err)
 
-// post
 
-// fetch(URL,{
-//     method: 'POST',
-//     body: JSON.stringify()
-// })
-//   .then(response => response.json())
-//   .then(data => console.log(data))
+//when load server
+window.onload = function (){
+    fetch(URL, {
+            method: 'POST',
+            body: JSON.parse(createNote),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(note => {
+            this.createElementItem(note)
+        })
+        .catch(err => console.log(err))
 
-fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify(),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
+        let btn = $(".btn")
+        btn.addEventListener('click', function(){
+            createNote();
+        })
+}
+// create a list
+function createNote(){
+     let title = $("#title").value;
+     let textarea = $("#textarea").value;
+
+     let note = {
+         title ,
+         textarea
+     }
+
+     $("#title").value = ""
+     $("#textarea").value = ""
+}
+
+
+
+function createElementItem(note){
+    const parentElement = $(".items");
+    let jumbotronDiv = document.createElement("div");
+    jumbotronDiv.classList = ["jumbotron text-white"];
+    let h1 = document.createElement("h1");
+    h1.classList = ["display-5"]
+    h1.innerText = note.title;
+    let p = document.createElement("p")
+    p.innerText = note.textarea;
+    jumbotronDiv.appendChild(h1);
+    jumbotronDiv.appendChild(p)
+    parentElement.appendChild(jumbotronDiv);
+}
+
+
+//create function querySelector
+function $(selector){
+    return document.querySelector(selector);
+}
+
+//auto increase height
+
+
+textarea.addEventListener('keydown', autoIncrease);
+
+function autoIncrease() {
+    var el = this;
+    setTimeout(function () {
+        el.style.cssText = 'height:auto; padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    }, 0);
+}
+
